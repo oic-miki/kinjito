b9a2c6b4a0abdf4ea6fbb620ee26b4401d6dd948
miki@oic.jp <miki@oic.jp>
2024-01-17 10:29:10 +0900

    プロジェクト ファイルを追加します。

---
+++ 金字塔/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Main
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+
+            new Program().recursive(int.Parse(s), 1);
+
+        }
+
+        private void recursive(int n, int i)
+        {
+
+            for (int blank = n - i; blank > 0; blank--)
+            {
+
+                Console.Write(" ");
+
+            }
+
+            for (int asterisk = 2 * i - 1; asterisk > 0; asterisk--)
+            {
+
+                Console.Write("*");
+
+            }
+
+            Console.WriteLine("");
+
+            if (n != i)
+            {
+
+                recursive(n, i + 1);
+
+            }
+
+        }
+
+    }
+
+}
